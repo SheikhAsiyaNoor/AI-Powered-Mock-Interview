8c483ce23f7f0cfe35f4c6e22edfa244d0dd72a9
Sheikh Asiya Noor <sheikhasiyanoor2317@gmail.com>
2026-08-07 22:31:29 +0530

    Use explicit custom CORS middleware for all routes and preflights

--- server/src/index.js
+++ server/src/index.js
@@ -10,14 +10,22 @@ const resumeRoutes = require("./routes/resume");
 connectDB();
 const app = express();
 
-const corsOptions = {
-    origin: (origin, callback) => callback(null, true),
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
-};
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if (origin) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    } else {
+        res.setHeader("Access-Control-Allow-Origin", "*");
+    }
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
-app.use(cors(corsOptions));
+    if (req.method === "OPTIONS") {
+        return res.status(200).end();
+    }
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
